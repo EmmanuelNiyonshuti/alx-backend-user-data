50ce2a8623a7f8a0b315a5628bdba3c5e6e8282c
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2024-09-14 10:30:05 +0200

    filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
-""" Filter log data."""
+""" finds and obfuscate certain fields within a log message. """
 import re
 
 
 def filter_datum(fields, redaction, message, separator):
-    """ finds and obfuscate certain fields within a log message. """
     pattern = f'({"|".join(fields)})=[^{separator}]*'
     return re.sub(pattern, f"\\1={redaction}", message)
-
