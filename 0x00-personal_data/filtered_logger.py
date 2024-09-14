9538731b819544e21fc7513332974f2272bc3d60
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2024-09-14 10:37:17 +0200

    filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 """ finds and obfuscate certain fields within a log message. """
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """ obfuscate or hide passed in fields in message"""
     pattern = f'({"|".join(fields)})=[^{separator}]*'
     return re.sub(pattern, f"\\1={redaction}", message)
