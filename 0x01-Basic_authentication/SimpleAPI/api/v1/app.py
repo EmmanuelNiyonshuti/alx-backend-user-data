c591ee915fc5dabdce6811145188428838ed84de
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2024-09-16 11:13:50 +0200

    templte for all authentication system that will be implemented

--- 0x01-Basic_authentication/SimpleAPI/api/v1/app.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/app.py
@@ -22,8 +22,13 @@ def not_found(error) -> str:
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    """ Unauthorized handler """
+    """ Unauthorized error handler """
     return jsonify({"error": "Unauthorized"}), 401
+
+@app.errorhandler(403)
+def forbidden(error):
+    """ Forbidden error handler """
+    return jsonify({"error": "Forbidden"}), 403
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
---
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
@@ -0,0 +1,18 @@
+from flask import request
+from typing import List, TypeVar
+from models.user import User
+
+
+class Auth:
+    """ """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ """
+        return None
