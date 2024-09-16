3db3ec80de3a93d049e734cad6ee2c371cb2c3b0
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2024-09-16 21:20:31 +0200

    Auth class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
-""" """
+""" implements auth class to manage api authentication. """
 from flask import request
 from typing import List, TypeVar
 from models.user import User
 
 
 class Auth:
-    """ """
+    """ Manage api authentication. """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ """
         if path is None or excluded_paths is None or len(excluded_paths) == 0:
             return True
         if not path.endswith("/"): path = path + "/"
-        if path not in excluded_paths: return True
         if path in excluded_paths: return False
+        else: return True
 
     def authorization_header(self, request=None) -> str:
         """ retrieves the value of the request authorization header.
