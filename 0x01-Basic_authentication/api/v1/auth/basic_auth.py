aa8881318bd4542708211da17204f4ab96862b06
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2024-09-17 06:21:12 +0200

    implemented extract_base64_authorization_header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,7 +2,28 @@
 """ implements Basic Auth """
 
 from api.v1.auth.auth import Auth
+import re
 
 
 class BasicAuth(Auth):
-    """ Basic Auth class"""
+    """ Basic Auth class """
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """ extract base64 string from the authorization header.
+        Args:
+            authorization_header (str) - auth string.
+        Return:
+         base64 string from authorization header if it is valid
+         otherwise None."""
+        if authorization_header is None or\
+                not isinstance(authorization_header, str):
+            return None
+        if not re.match(r'^Basic\s+[A-Za-z0-9+/]+={0,2}$',
+                        authorization_header):
+
+            return None
+
+        auth_list = authorization_header.split(" ")
+        auth_str = auth_list[1]
+        return auth_str
