06880b99157ed91bcaaca0da59c3639c5af936c0
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2024-09-19 17:09:23 +0200

    6. Use Session ID for identifying a User

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,6 +2,7 @@
 """ Session authentication implementation."""
 import uuid
 from api.v1.auth.auth import Auth
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -24,7 +25,7 @@ class SessionAuth(Auth):
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
         """
-        retrieves user_id from user_id_by_session_id
+        retrieves user_id from user_id_by_session_id dictinary
         based on passed in session_id.
         Args:
             session_id (str)
@@ -33,4 +34,26 @@ class SessionAuth(Auth):
         """
         if session_id is None or not isinstance(session_id, str):
             return None
-        return SessionAuth.user_id_by_session_id.get(session_id)
+        for k, v in SessionAuth.user_id_by_session_id.items():
+            if k == session_id:
+                user_id = v
+        user_id = SessionAuth.user_id_by_session_id.get(session_id)
+        return user_id
+
+    def current_user(self, request=None):
+        """
+        retrieves a user based on a cookie value.
+        Args:
+            request: The http request object.
+        Return:
+            user object if a valid session exists, None otherwise.
+        """
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return None
+
+        for v in self.user_id_by_session_id.values():
+            user_id = v
+        if user_id is None:
+            return None
+        return User.get(user_id)
