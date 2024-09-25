cfbcf83f039a8194bd49bf9d25a5b62f33668572
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2024-09-25 18:27:00 +0200

    fixed a typo

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -24,7 +24,7 @@ def _hash_password(password: str) -> bytes:
     return hashed_password
 
 
-def _generate_uuid(self) -> str:
+def _generate_uuid() -> str:
     """ generates a uuid4 str"""
     return str(uuid.uuid4())
 
