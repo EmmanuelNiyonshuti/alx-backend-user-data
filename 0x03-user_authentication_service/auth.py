91bc32763deaf8e7cb81c9f56a53e4641a85a240
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2024-09-25 18:06:02 +0200

    valid_login

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,6 +3,8 @@
 handle user auth.
 """
 import bcrypt
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 from db import DB
 from user import User
 
@@ -43,3 +45,20 @@ class Auth:
         hashed_password = _hash_password(password)
         user = self._db.add_user(email, hashed_password)
         return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """
+        validate user credentials.
+        Args:
+            email (str) - user email.
+            password (str) - user password.
+        Return:
+            bool.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                password_bytes = password.encode("utf-8")
+                return bcrypt.checkpw(password_bytes, user.hashed_password)
+        except (InvalidRequestError, NoResultFound):
+            return False
