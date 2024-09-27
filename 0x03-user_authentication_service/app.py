80811b96444b34758e4fd348a6b1f1052b173313
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2024-09-27 02:13:11 +0200

    mandatory tasks

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -29,7 +29,7 @@ def users():
     password = request.form.get("password")
     try:
         user = AUTH.register_user(email, password)
-        return jsonify({"email": email, "message": "user created"})
+        return jsonify({"email": email, "message": "user created"}), 201
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
 
@@ -56,6 +56,42 @@ def logout():
         abort(403)
     AUTH.destroy_session(user.id)
     return redirect("/")
+
+
+@app.route("/profile")
+def profile():
+    """ User profile. """
+    session_id = request.cookies.get("session_id")
+    if session_id is None or not isinstance(session_id, str):
+        abort(403)
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        abort(403)
+    return jsonify({"email": user.email}), 200
+
+
+@app.route("/reset_password", methods=["POST"])
+def get_reset_password_token():
+    """ sends a password reset token"""
+    email = request.form.get("email")
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": reset_token}), 200
+    except ValueError:
+        abort(403)
+
+
+@app.route("/reset_password", methods=["PUT"])
+def update_password():
+    """ reset user password"""
+    email = request.form.get("email")
+    reset_token = request.form.get("reset_token")
+    new_password = request.form.get("new_password")
+    try:
+        AUTH.update_password(reset_token, new_password)
+        return jsonify({"email": email, "message": "Password updated"}), 200
+    except ValueError:
+        abort(403)
 
 
 if __name__ == "__main__":
--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -48,7 +48,7 @@ class Auth:
         session = self._db._session
         user = session.query(User).filter_by(email=email).first()
         if user:
-            raise ValueError(f"User {email} already exists")
+            raise ValueError("User {} already exists".format(email))
         hashed_password = _hash_password(password)
         new_user = self._db.add_user(email, hashed_password)
         return new_user
@@ -115,3 +115,40 @@ class Auth:
             return None
         except ValueError:
             pass
+
+    def get_reset_password_token(self, email: str) -> str:
+        """
+        set user password reset token.
+        Args:
+            email (str) - user email.
+        Return:
+            str.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            if user is None:
+                raise ValueError
+            reset_token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=reset_token)
+            return reset_token
+        except (NoResultFound, InvalidRequestError):
+            pass
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """
+        update user password.
+        Args:
+            reset_token (str)
+            password (str)
+        Return:
+            None.
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            hashed_pwd = _hash_password(password)
+            self._db.update(
+                            user.id,
+                            hashed_password=hashed_pwd,
+                            reset_token=None)
+        except NoResultFound:
+            raise ValueError
